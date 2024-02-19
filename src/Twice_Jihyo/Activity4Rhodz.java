38fee788e5ab38957f9b0a56000eeaadd2c48ca9
Rhodz <Rhodz@DESKTOP-INQKN1K>
2024-02-19 18:21:38 +0800

    _zyozyo

---
+++ src/Twice_Jihyo/Activity4Rhodz.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Twice_Jihyo;
+
+/**
+ *
+ * @author Rhodz
+ */
+public class Activity4Rhodz {
+    
+    public static void main(String[] args) {
+        
+        int entranceScore = 75;
+        double gpa = 1.0;
+        
+        if (gpa > entranceScore) {
+            System.out.println("Eligible for admission.");
+        }
+        else {
+            System.out.println("Not eligible for admission.");
+        }
+    }
+}
